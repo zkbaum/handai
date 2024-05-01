30d040cb2654d1cef04b2fa928dec96ced84ec72
zkbaum <zach.kirschenbaum@gmail.com>
2024-04-30 22:03:23 -0400

    update analysis script to support ron's format

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -5,6 +5,7 @@ Script to analyze results from a hand AI experiment.
 import pandas as pd
 from enum import Enum
 from private import get_result_csvpath_for_experiment, ROOT_DIR
+import csv
 
 
 class Experiment(Enum):
@@ -71,7 +72,7 @@ def _parse_inference_results_df(df):
     )
     df.reset_index(inplace=True)
 
-    # Average the 3 attempts.
+    # average the 3 attempts
     df["chatgpt_average_correct_percentage"] = df[
         [
             "chatgpt_attempt0_correct",
@@ -80,7 +81,7 @@ def _parse_inference_results_df(df):
         ]
     ].mean(axis=1)
 
-    return df[
+    df = df[
         [
             "question_type",
             "human_correct_percentage",
@@ -88,26 +89,135 @@ def _parse_inference_results_df(df):
         ]
     ]
 
+    return df
 
-dfs = []
-for exp in list(Experiment):
-    filepath = get_result_csvpath_for_experiment(exp)
-    df = pd.read_csv(filepath)
-    # If you only ran it one time.
-    # if exp == Experiment.RAG_ZERO_SHOT:
-    #     df["chatgpt_answer_1"] = df["chatgpt_answer_0"]
-    #     df["chatgpt_answer_2"] = df["chatgpt_answer_0"]
-    df = _parse_inference_results_df(df)
-    df["experiment_name"] = exp.value
-    dfs.append(df)
 
-joined = pd.concat(dfs, ignore_index=True)
-joined = joined.replace("ContentType.TEXT_ONLY", "Text only")
-joined = joined.replace("ContentType.TEXT_AND_IMAGES", "Image based")
+def _compute_averages():
+    dfs = []
+    for exp in list(Experiment):
+        filepath = get_result_csvpath_for_experiment(exp)
+        df = pd.read_csv(filepath)
+        # If you only ran it one time.
+        # if exp == Experiment.RAG_ZERO_SHOT:
+        #     df["chatgpt_answer_1"] = df["chatgpt_answer_0"]
+        #     df["chatgpt_answer_2"] = df["chatgpt_answer_0"]
+        df = _parse_inference_results_df(df)
+        df["experiment_name"] = exp.value
+        dfs.append(df)
 
-output_path = f"{ROOT_DIR}/out/analysis/stats.csv"
-joined.to_csv(output_path, index=True)
-print(f"wrote results to {output_path}")
+    joined = pd.concat(dfs, ignore_index=True)
+
+    return joined
+
+
+def _compute_averages_per_attempt_slice_by_question_type():
+    results = []
+    for exp in list(Experiment):
+        filepath = get_result_csvpath_for_experiment(exp)
+        df = pd.read_csv(filepath)
+
+        df["chatgpt_attempt0_correct"] = df["chatgpt_answer_0"] == df["actual_answer"]
+        df["chatgpt_attempt1_correct"] = df["chatgpt_answer_1"] == df["actual_answer"]
+        df["chatgpt_attempt2_correct"] = df["chatgpt_answer_2"] == df["actual_answer"]
+
+        question_type_counts = df["question_type"].value_counts()
+        df["num_questions"] = df["question_type"].map(question_type_counts) / 100
+        df = (
+            df.groupby("question_type")[
+                [
+                    "chatgpt_attempt0_correct",
+                    "chatgpt_attempt1_correct",
+                    "chatgpt_attempt2_correct",
+                    "num_questions",
+                ]
+            ].mean()
+            * 100
+        )
+        df.reset_index(inplace=True)
+        df["num_questions"] = df["num_questions"].astype(int)
+        df = df.replace("ContentType.TEXT_ONLY", "Text only")
+        df = df.replace("ContentType.TEXT_AND_IMAGES", "Image based")
+
+        exp_name = exp.value
+        for _, row in df.iterrows():
+            question_type = row["question_type"]
+            num_questions = row["num_questions"]
+            for attempt in [0, 1, 2]:
+                col = f"chatgpt_attempt{attempt}_correct"
+                avg = row[col]
+                results.append([exp_name, question_type, num_questions, attempt, avg])
+
+    return results
+
+
+def _compute_averages_per_attempt(slice_by_question_type):
+    if slice_by_question_type:
+        return _compute_averages_per_attempt_slice_by_question_type()
+
+    results = []
+    for exp in list(Experiment):
+        filepath = get_result_csvpath_for_experiment(exp)
+        df = pd.read_csv(filepath)
+
+        df["chatgpt_attempt0_correct"] = df["chatgpt_answer_0"] == df["actual_answer"]
+        df["chatgpt_attempt1_correct"] = df["chatgpt_answer_1"] == df["actual_answer"]
+        df["chatgpt_attempt2_correct"] = df["chatgpt_answer_2"] == df["actual_answer"]
+
+        num_questions = len(df)
+        df = (
+            df[
+                [
+                    "chatgpt_attempt0_correct",
+                    "chatgpt_attempt1_correct",
+                    "chatgpt_attempt2_correct",
+                ]
+            ].mean()
+            * 100
+        ).to_frame(name="Value")
+
+        attempt0_avg = df.loc["chatgpt_attempt0_correct", "Value"]
+        attempt1_avg = df.loc["chatgpt_attempt1_correct", "Value"]
+        attempt2_avg = df.loc["chatgpt_attempt2_correct", "Value"]
+
+        exp_name = exp.value
+        question_type = "ALL"
+        results.append([exp_name, question_type, num_questions, 0, attempt0_avg])
+        results.append([exp_name, question_type, num_questions, 1, attempt1_avg])
+        results.append([exp_name, question_type, num_questions, 2, attempt2_avg])
+
+    return results
+
+
+def _write_df_to_csv(df, filename):
+    df = df.replace("ContentType.TEXT_ONLY", "Text only")
+    df = df.replace("ContentType.TEXT_AND_IMAGES", "Image based")
+
+    output_path = f"{ROOT_DIR}/out/analysis/{filename}"
+    df.to_csv(output_path, index=True)
+    print(f"wrote results to {output_path}")
+
+
+def _write_list_to_csv(results, filename):
+    output_path = f"{ROOT_DIR}/out/analysis/{filename}"
+
+    with open(output_path, mode="w", newline="") as file:
+        writer = csv.writer(file)
+
+        header = ["exp_name", "question_type", "num_questions", "attempt", "average"]
+        writer.writerow(header)
+        for result in results:
+            writer.writerow(result)
+    print(f"wrote results to {output_path}")
+
+
+averages_df = _compute_averages()
+_write_df_to_csv(averages_df, "average-stats.csv")
+
+averages_per_attempt_df = _compute_averages_per_attempt(slice_by_question_type=True)
+_write_list_to_csv(averages_per_attempt_df, "per-attempt-stats-by-question-type.csv")
+
+averages_per_attempt_df = _compute_averages_per_attempt(slice_by_question_type=False)
+_write_list_to_csv(averages_per_attempt_df, "per-attempt-stats.csv")
 
 # For some reason (likely because of the circular imports), this module gets
 # executed twice. So we manually add an exit.
